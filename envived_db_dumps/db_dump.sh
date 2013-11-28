a0db257bea848ee3abfa0d388910ac75ee514175
Alex Sorici <alex.sorici@gmail.com>
2013-11-28 14:48:45 +0100

    Updated db_dump.sh script to take database name, database user name and database user password as command line arguments

--- envived_db_dumps/db_dump.sh
+++ envived_db_dumps/db_dump.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-DB_USER="envived"
-DB_PASS="GroundAce55"
-DB_NAME="envived"
-
-if [ $# -lt 1 ]
+if [ $# -lt 4 ]
 then
-    echo "Error in $0 - not enough arguments supplied. Usage: $0 [dump|restore] [db_file_name]"
+    echo "Error in $0 - not enough arguments supplied. Usage: $0 dump|restore <DB_NAME> <DB_USER> <DB_PASS> [db_file_name]"
 exit 1
 fi
+
+DB_NAME=$2
+DB_USER=$3
+DB_PASS=$4
 
 if [ "$1" == "dump" ]
 then
     filename="envived_dump_"$(date +%Y_%m_%d_%H_%M_%S)".sql"
     mysqldump -u $DB_USER -p$DB_PASS $DB_NAME > $filename
 else
-    if [ "$1" == "restore" -a $# -eq 2 ]
+    if [ "$1" == "restore" -a $# -eq 5 ]
     then
-        mysql -u $DB_USER -p$DB_PASS $DB_NAME < $2
+        mysql -u $DB_USER -p$DB_PASS $DB_NAME < $5
     else
-        echo "Error in $0 - wrong arguments supplied. Usage: $0 [dump|restore] [db_file_name]"
+        echo "Error in $0 - wrong arguments supplied. Usage: $0 restore <DB_NAME> <DB_USER> <DB_PASS> <db_file_name>"
     fi
 fi
