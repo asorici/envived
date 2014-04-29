b0d650e2e96a7aed87570da76acfb2c3f72b9f2d
Alex Tomescu <alex.dan.tomescu@gmail.com>
2014-04-29 14:11:34 +0300

    modified message_provider_test.py to contain the new messages

--- settings/messaging.py
+++ settings/messaging.py
@@ -34,7 +34,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'envived',                   # Or path to database file if using sqlite3.
         'USER': 'envived',                         # Not used with sqlite3.
-        'PASSWORD': 'GroundAce55',                 # Not used with sqlite3.
+        'PASSWORD': '215894',                 # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
