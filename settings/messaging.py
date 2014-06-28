43d4bd27fe2100bb5e8059728729dd604f153921
Alex Tomescu <alex.dan.tomescu@gmail.com>
2014-06-29 00:52:07 +0200

    wip

--- settings/messaging.py
+++ settings/messaging.py
@@ -40,7 +40,7 @@ DATABASES = {
     },
 }
 
-RETRIEVE_MESSAGE_TIMEOUT = 300
+RETRIEVE_MESSAGE_TIMEOUT = 25
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
